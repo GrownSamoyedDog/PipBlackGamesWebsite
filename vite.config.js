27c0a80aa36cc03240f696259bb0427ba289e54e
GrownSamoyedDog <kserdutz@gmail.com>
2026-04-14 08:43:45 -0700

    changed folder to docs

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/<your-repo-name>/", // for project pages
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
